89216c8636a1d1cf0de4caa8cb26e4c78239f1c7
kcaakash49 <kcaakash4910@gmail.com>
2024-11-19 20:39:14 +0545

    logo and footer added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,27 +11,31 @@ import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
 import Search from "./pages/Search";
 import Footer from "./components/Footer";
+import UserLayout from "./layout/UserLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/listing/:listingId" element={<Listing />} />
-      <Route path="/search" element={<Search />} />
-      <Route element={<PrivateRoute />}>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/create-listing" element={<CreateListing />} />
-        <Route path="/update-listing/:listingId" element={<UpdateListing />} />
-      </Route>
-    </Routes>
-    <Footer/>
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/listing/:listingId" element={<Listing />} />
+          <Route path="/search" element={<Search />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />} />
+            <Route
+              path="/update-listing/:listingId"
+              element={<UpdateListing />}
+            />
+          </Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Frontend/src/layout/UserLayout.jsx
+++ Frontend/src/layout/UserLayout.jsx
@@ -1,9 +1,17 @@
-
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const UserLayout = () => {
   return (
-    <div>UserLayout</div>
-  )
-}
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-export default UserLayout
+export default UserLayout;
