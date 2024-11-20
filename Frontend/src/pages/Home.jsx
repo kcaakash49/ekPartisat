94f0ec5bfc2fef06f4ac3874fd6514430428baba
kcaakash49 <kcaakash4910@gmail.com>
2024-11-20 20:58:50 +0545

    init

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -49,8 +49,8 @@ export default function Home() {
     <div>
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-7xl mx-auto'>
-        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+        <h1 className='text-white font-bold text-3xl lg:text-6xl'>
+          Find your next <span className='text-slate-400'>perfect</span>
           <br />
           place with ease
         </h1>
@@ -62,7 +62,7 @@ export default function Home() {
         </div>
         <Link
           to={'/search'}
-          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
+          className='text-xs sm:text-sm text-blue-300 font-bold hover:underline'
         >
           Let's get started...
         </Link>
